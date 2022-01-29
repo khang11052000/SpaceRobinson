9eb92a6e47be021c5433ea10ace23c1a4aa4773f
Khang <nguyenhoangduykhang11052000@gmail.com>
2022-01-29 23:25:05 +0700

    Up code lan dau

---
+++ Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 offset;
+
+    [Range(0, 10)] 
+    public float smoothFactor;
+
+    private void FixedUpdate()
+    {
+        Follow();
+    }
+
+    void Follow()
+    {
+        Vector3 targetPosition = target.position + offset;
+        Vector3 smothPosition = Vector3.Lerp(transform.position, targetPosition, smoothFactor * Time.fixedDeltaTime);
+        transform.position = smothPosition;
+    }
+}
